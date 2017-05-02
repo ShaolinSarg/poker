9dee2617c10512d7206c69829ab14bebaa942a0e
David Sarginson <david.sarginson@digital.hmrc.gov.uk>
2017-05-02 12:01:26 +0100

    using thread macro to make it easier to read

--- src/poker/cards.clj
+++ src/poker/cards.clj
@@ -6,7 +6,9 @@
 (def values '(:2 :3 :4 :5 :6 :7 :8 :9 :10 :J :Q :K :A))
 (def new-deck (for [v values s suits] (Card. v s)))
 
-(def straights (map #(into #{} %) (partition 5 1 (conj values :A))))
+(def straights (->> (conj values :A)
+                    (partition 5 1)
+                    (map #(into #{} %))))
 
 
 (defn create-deck [] 
@@ -17,8 +19,12 @@
 
 (defn highCard [card1 card2]
   (let [c1 (:value card1) c2 (:value card2)
-        t1 (count (take-while #(not= % c1) values))
-        t2 (count (take-while #(not= % c2) values))]
+        t1 (->> values
+                (take-while #(not= % c1))
+                (count))
+        t2 (->> values
+                (take-while #(not= % c2))
+                (count))]
     (cond
       (> t1 t2) card1
       (> t2 t1) card2
